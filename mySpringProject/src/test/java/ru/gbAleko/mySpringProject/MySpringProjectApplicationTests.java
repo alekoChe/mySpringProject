9a51b1d447a8f3e852bf36c334024c59177deded
alekoChe <aleko2117bis@gmail.com>
2022-09-13 09:59:15 +0300

    Initial commit

---
+++ mySpringProject/src/test/java/ru/gbAleko/mySpringProject/MySpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gbAleko.mySpringProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
